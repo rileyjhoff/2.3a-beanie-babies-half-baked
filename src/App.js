0e55e76dfc599add6208bdb65960fe7c3b2abcd5
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-09 12:49:46 -0700

    instead of reordering Routes, exact paths are defined. solves the same problem

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ export default function App() {
     <Router>
       <div className="App">
         <Switch>
-          <Route path="/beanies/:id">
-            {/* this route should point to a particular beanie baby by id and render that specific BeanieDetail page */}
-            <BeanieDetail />
-          </Route>
-          <Route path="/">
+          <Route exact path="/">
             {/* this home page route should list all the beanies */}
             <BeaniesPage />
+          </Route>
+          <Route exact path="/beanies/:id">
+            {/* this route should point to a particular beanie baby by id and render that specific BeanieDetail page */}
+            <BeanieDetail />
           </Route>
         </Switch>
       </div>

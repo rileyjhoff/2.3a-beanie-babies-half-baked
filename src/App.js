c84885523683fbe5b567a50e2dd809fd97de00c7
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-09 11:57:53 -0700

    App calls BeaniePage and BeanieDetail components in the two Router switches and Routes pathes for both pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import BeaniesPage from './BeaniesPage';
+import BeaniesPage from './BeaniesPage';
 // import BeanieDetail from './BeanieDetail';
 
 export default function App() {
@@ -8,9 +8,13 @@ export default function App() {
     <Router>
       <div className="App">
         <Switch>
-          <Route>{/* this home page route should list all the beanies */}</Route>
-          <Route>
-            {/* this route should point to a particulat beanie baby by id and render that specific BeanieDetail page */}
+          <Route path="/">
+            {/* this home page route should list all the beanies */}
+            <BeaniesPage />
+          </Route>
+          <Route path="/beanies/:id">
+            {/* this route should point to a particular beanie baby by id and render that specific BeanieDetail page */}
+            {/* <BeanieDetail /> */}
           </Route>
         </Switch>
       </div>
